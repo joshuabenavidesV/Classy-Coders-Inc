773c03861cfa6823598211a06301371fbc8daa49
LaneLR <laneleerichardson@gmail.com>
2025-05-07 12:01:06 -0500

    added Manager.js

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,6 +1,22 @@
-class Manager {
+// import Employee from "./Employee"
+const { Employee } = require("./Employee")
 
+class Manager extends Employee {
+    #employeesManaged
+    constructor(name, position, salary, department, employeesManaged = []) {
+        super(name, position, salary)
+        this.department = department;
+        this.#employeesManaged = employeesManaged;
+    }
 
+    getEmployeesManaged() {
+        return this.#employeesManaged
+    }
+
+    addEmployeeManaged(employee) {
+        return this.#employeesManaged.push(employee)
+    }
+    
 
     
 }
